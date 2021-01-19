7b52b56e6b9b399de8828b60dde94bcfee44c2b1
Pedro Ribeiro <pedro@workwithgrid.com>
2021-01-18 22:31:49 -0500

    clean up

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,8 +1,4 @@
-defmodule User do
-  defstruct age: 0
-end
-
-
+ 
 defmodule Fizzbuzz do
   @moduledoc """
   Documentation for `Fizzbuzz`.
@@ -13,20 +9,21 @@ defmodule Fizzbuzz do
 
   ## Examples
 
-      iex> Fizzbuzz.check(3)
-      "Fizz"
+      iex> Fizzbuzz.convert([3])
+      ["Fizz"]
 
       iex> Fizzbuzz.convert([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
       ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16"]
 
   """
-  def check(i) when rem(i, 15) == 0 and i > 0,  do: "FizzBuzz"
-  def check(i) when rem(i, 3) == 0 and i > 0, do: "Fizz"
-  def check(i) when rem(i, 5) == 0 and i > 0, do: "Buzz"
-  def check(i),  do:  "#{i}"
+
+  def convert(l), do: Enum.map(l, &change/1)
+  defp change(i) when rem(i, 15) == 0 and i > 0,  do: "FizzBuzz"
+  defp change(i) when rem(i, 3) == 0 and i > 0, do: "Fizz"
+  defp change(i) when rem(i, 5) == 0 and i > 0, do: "Buzz"
+  defp change(i),  do:  "#{i}"
 
 
-  def convert(l), do: Enum.map(l, &check/1)
 end
 
 
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -3,21 +3,26 @@ defmodule FizzbuzzTest do
   doctest Fizzbuzz
 
   test "if multiple of 3" do
-    assert Fizzbuzz.check(3) == "Fizz"
+    assert  Fizzbuzz.convert([3]) == ["Fizz"]
+    assert  Fizzbuzz.convert([6]) == ["Fizz"]
+    
   end
   test "if multiple of 5" do
-      assert Fizzbuzz.check(5) == "Buzz"
+    assert  Fizzbuzz.convert([5]) == ["Buzz"]
+    assert  Fizzbuzz.convert([10]) == ["Buzz"]
   end
   test "if multiple of 5 and 3" do
-        assert Fizzbuzz.check(15) == "FizzBuzz"
+    assert Fizzbuzz.convert([15]) == ["FizzBuzz"]
+    assert Fizzbuzz.convert([30]) == ["FizzBuzz"]
+    assert Fizzbuzz.convert([45]) == ["FizzBuzz"]
   end
-  test "everything else  " do
-    assert Fizzbuzz.check(0) == "0"
-    assert Fizzbuzz.check(1) == "1"
-    assert Fizzbuzz.check(4) == "4"
+  test "everything else returns value as string " do
+    assert Fizzbuzz.convert([0]) == ["0"]
+    assert Fizzbuzz.convert([1]) == ["1"]
+    assert Fizzbuzz.convert([4]) == ["4"]
   end
 
-  test "has the convert function that will receive a list and return it converted " do
+  test "pass a list and returns modified " do
     expected = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16"]
     assert  Fizzbuzz.convert(1..16) == expected
   end
